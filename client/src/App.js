8c81ca176ed5a133e874827d24d6fac612ef5690
Peter D'Elia <petertdelia@gmail.com>
2021-09-03 13:05:18 -0400

    multiple clients now synced

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
 import React, { useRef } from "react";
 import Editor from "@monaco-editor/react";
+import * as Y from 'yjs';
+import { WebsocketProvider } from 'y-websocket';
 import { MonacoBinding } from 'y-monaco';
 import { FileUploader } from './components/Uppy.js';
 
 function App() {
   const editorRef = useRef(null);
+  const ydocument = new Y.Doc();
+  const provider = new WebsocketProvider(`ws://localhost:1234`, 'monaco', ydocument);
+  const type = ydocument.getText('monaco');
 
   function handleEditorDidMount(editor, monaco) {
     // here is the editor instance
     // you can store it in `useRef` for further usage
     editorRef.current = editor; 
+    const monacoBinding = new MonacoBinding(type, editorRef.current.getModel(), new Set([editorRef.current]), provider.awareness);
   }
 
   return (
